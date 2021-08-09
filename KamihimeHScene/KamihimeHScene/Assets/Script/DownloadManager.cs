5e3b8bf5dd9ae2d1a67087937f118f020e61e19a
imgRenko <1349661047@qq.com>
2021-08-09 12:35:13 +0800

    An almost final update

--- KamihimeHScene/KamihimeHScene/Assets/Script/DownloadManager.cs
+++ KamihimeHScene/KamihimeHScene/Assets/Script/DownloadManager.cs
@@ -32,6 +32,7 @@ public class PlayerManager {
     public Dictionary<string, AudioClip> voiceClips = new Dictionary<string, AudioClip>();
     public Dictionary<string, Texture2D> imageCollections = new Dictionary<string, Texture2D>();
     public int animFPS = 1;
+    public bool Opened = false;
 }
 public class DownloadInfo {
     public string Name;
@@ -55,11 +56,14 @@ public class DownloadManager : MonoBehaviour
 
     private Text Message, CharName;
 
-    public string yourName="赞美太阳";
-
+    [HideInInspector]
     public Slider seqSlider, talSlider;
 
+    public string yourName = "赞美太阳";
+
     public Text downloadProgress;
+
+    public Button closeButton, downloadButton;
 
     private List<DownloadInfo> downloadInfos = new List<DownloadInfo>();
 
@@ -110,11 +114,25 @@ public class DownloadManager : MonoBehaviour
         sceneContent.texture = www.texture;
 
     }
+
+    public void BanButton(bool Ban) {
+        closeButton.interactable = !Ban;
+        downloadButton.interactable = !Ban;
+    }
+
     public IEnumerator DownloadJsonTest()
     {
         string url = string.Format("https://static-r.kamihimeproject.net/scenarios/c2d/c46/{0}/{1}", contentFolderName, "scenario.json");//jsonPath;
+
+        DownloadInfo info = new DownloadInfo();
+        info.Name = url;
+        info.Progress = 0;
+        downloadInfos.Add(info);
+
         WWW www = new WWW(url);
         yield return www;
+  
+
         if (www.isDone)
         {
             string content = "{\"sequence\":" + www.text + "}";
@@ -124,6 +142,15 @@ public class DownloadManager : MonoBehaviour
             content = content.Replace(",\n\n", "");
             Debug.Log(content);
             sequenceData = JsonUtility.FromJson<Sequence>(content);
+            downloadInfos.Remove(info);
+            if (www.error != null)
+            {
+                Debug.LogError(www.error);
+                Message.text = www.error;
+            }      
+            else
+                Message.text = "已经装载json文件，再点击一次下载按钮。";
+
         }
     }
 
@@ -136,8 +163,10 @@ public class DownloadManager : MonoBehaviour
         info.Name = url;
         info.Progress = 0;
         downloadInfos.Add(info);
+
         float Progess = 0;
         string Info = "";
+
         if (isImage)
         {
             WWW www = new WWW(url);
@@ -168,6 +197,7 @@ public class DownloadManager : MonoBehaviour
             yield return www.SendWebRequest();
             while (!www.isDone) {
                 Progess = www.downloadProgress;
+                info.Progress = Progess;
                 yield return null;
             }
             
@@ -186,7 +216,7 @@ public class DownloadManager : MonoBehaviour
                 var samples = new float[mpgFile.Length];
                 mpgFile.ReadSamples(samples, 0, (int)mpgFile.Length);
 
-                var clip = AudioClip.Create("foo", samples.Length, mpgFile.Channels, mpgFile.SampleRate, false);
+                var clip = AudioClip.Create("voice", samples.Length, mpgFile.Channels, mpgFile.SampleRate, false);
                 clip.SetData(samples, 0);
                 playerManager.voiceClips.Add(path, clip); // source.clip = clip;
             }
@@ -204,25 +234,38 @@ public class DownloadManager : MonoBehaviour
         playerManager.voiceClips.Clear();
         playerManager.currentSubSequenceIndex = 0;
         playerManager.currentSequenceIndex = 0;
+        sequenceData.sequence = new SequenceData[0] ;
         sceneContent.texture = Texture2D.whiteTexture;
         CharName.text = "";
         Message.text = "";
+        playerManager.Opened = false;
     }
 
     public void DownloadCharacterData() {
-        Close();
-        StartCoroutine(DownloadJsonTest());
-        Message.text = "已经装载json文件，再点击一次下载按钮。";
-        foreach (var subSequenceData in sequenceData.sequence) {
-            if (subSequenceData.film != null)
-               StartCoroutine( DownloadAsset(subSequenceData.film,true));
-
-            foreach (var talkingData in subSequenceData.talk)
-                if (talkingData.voice != null)
-                    StartCoroutine(DownloadAsset(talkingData.voice,false));
-
+        if (playerManager.Opened) 
+            Close();
+        if (sequenceData.sequence.Length == 0)
+        {
+            StartCoroutine(DownloadJsonTest());
+           
         }
-        seqSlider.maxValue = DownloadManager.Instance.sequenceData.sequence.Length - 1;
+        else
+        {
+            CharName.text = "";
+            Message.text = "";
+            foreach (var subSequenceData in sequenceData.sequence)
+            {
+                if (subSequenceData.film != null)
+                    StartCoroutine(DownloadAsset(subSequenceData.film, true));
+
+                foreach (var talkingData in subSequenceData.talk)
+                    if (talkingData.voice != null)
+                        StartCoroutine(DownloadAsset(talkingData.voice, false));
+
+            }
+            seqSlider.maxValue = DownloadManager.Instance.sequenceData.sequence.Length - 1;
+            playerManager.Opened = true;
+        }
     }
 
 
@@ -273,7 +316,9 @@ public class DownloadManager : MonoBehaviour
     }
     private void Update()
     {
-        downloadProgress.gameObject.SetActive(downloadInfos.Count != 0);
+        bool downloadingContent = downloadInfos.Count != 0;
+        downloadProgress.gameObject.SetActive(downloadingContent);
+        BanButton(downloadingContent);
         if (downloadInfos.Count != 0)
         {
             downloadProgress.text = "";
